a654e93a7fde6ffa83ffc19bef4c67e445a1ff16
gonzavadone <gonzalo.vadone@davinci.edu.ar>
2024-05-06 14:43:35 -0300

    saludo

--- src/ejemplo/Main.java
+++ src/ejemplo/Main.java
@@ -1,9 +1,14 @@
 package ejemplo;
 
+import javax.swing.JOptionPane;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		JOptionPane.showMessageDialog(null, "Hola");
+		
 
 	}
 
